4f207d87c992f606f0a0aa34b6ea0c5020811498
asomwanshi37 <asomwanshi37@gmail.co.com>
2022-01-20 22:46:37 +0530

    [Akshay] Add: hired_candidates Data Added

---
+++ Lmsdata_insert queries.sql
@@ -0,0 +1,12 @@
+----- Hiring Microservice_Candidates-------
+INSERT INTO  hired_candidates(first_name,middle_name,last_name,email,mobile_num,hired_city,hired_date,degree,aggr_per,current_pincode,permanent_pincode,hired_lab,attitude_remark,communication_remark,knowledge_remark, status, creator_stamp,creator_user)
+VALUES('Ram','Vivek','varma','ram@gmail.com',7507062301,'pune','2022-01-19 14:36:20','BE',68.25,412315,315215,'TATA','Good','Very_Good','Good','ok','2022-01-19 00:00:00',1);
+INSERT INTO  hired_candidates(first_name,middle_name,last_name,email,mobile_num,hired_city,hired_date,degree,aggr_per,current_pincode,permanent_pincode,hired_lab,attitude_remark,communication_remark,knowledge_remark, status, creator_stamp,creator_user)
+VALUES('Vinod','Vishal','Sarma','vinod@gmail.com',9765658974,'Mumbai','2022-01-18 14:20:20','BE',78.25,412315,315215,'Jio','Good','Good','Good','Good','2012-02-17 00:00:00',5);
+INSERT INTO  hired_candidates(first_name,middle_name,last_name,email,mobile_num,hired_city,hired_date,degree,aggr_per,current_pincode,permanent_pincode,hired_lab,attitude_remark,communication_remark,knowledge_remark, status, creator_stamp,creator_user)
+VALUES('Karan','Pravin','Satav','karan12@gmail.com',9987985546,'Nashik','2021-11-28 04:00:00','BE',98.55,512315,615215,'Wipro','Good','Good','Good','Good','2012-02-17 11:02:05',4);
+INSERT INTO  hired_candidates(first_name,middle_name,last_name,email,mobile_num,hired_city,hired_date,degree,aggr_per,current_pincode,permanent_pincode,hired_lab,attitude_remark,communication_remark,knowledge_remark, status, creator_stamp,creator_user)
+VALUES('Vinit','Vasant','Desai','vvdesai55@gmail.com',8888888888,'Thane','2011-11-28 05:20:50','BSC',58.78,600288,,431518,'Knite','Good','Good','Good','Good','2018-02-17 15:02:05',8);
+INSERT INTO  hired_candidates(first_name,middle_name,last_name,email,mobile_num,hired_city,hired_date,degree,aggr_per,current_pincode,permanent_pincode,hired_lab,attitude_remark,communication_remark,knowledge_remark, status, creator_stamp,creator_user)
+VALUES('Ankush','Shamrao','Ambad','ankush98@gmail.com','9598789789','Kalyan','2019-10-20 06:20:40','MSC','68.88','600288','431518','Honda','V_Good','Good','Good','Good','2016-05-27 18:03:55','7');
+
