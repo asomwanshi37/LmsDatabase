bc48699864f580af3685a30e5b118f4ee9c8c8e4
asomwanshi37 <asomwanshi37@gmail.co.com>
2022-01-21 16:58:51 +0530

    [Akshay] Add: Refactoring  done

--- MySQL subquery.sql
+++ MySQL subquery.sql
@@ -1,5 +1,3 @@
-
------------------------MySQL Subqueries----------------------------
 select first_name,last_name from fellowship_candidates where email in (select email  from fellowship_candidates where  hired_city = 'kaij');
 
 select EmployeeID, First_Name,Salary from Employee_Salary  where  Salary = (select min(Salary) from Employee_Salary);
