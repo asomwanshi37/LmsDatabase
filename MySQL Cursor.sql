64f174c8b01c519a5450804b81d292206c838ff8
asomwanshi37 <asomwanshi37@gmail.co.com>
2022-01-24 22:32:32 +0530

    [Akshay] Add: MySQL Cursor Added

--- MySQL Cursor.sql
+++ MySQL Cursor.sql
@@ -14,19 +14,23 @@ delimiter ;
 
 describe employee_salary;
 
-call cursor2();
+call cursor12();
 
 
-delimiter //
-create procedure cursor20 ()
+delimiter $$
+create procedure pass_Emp()
 begin
-declare i int;
-declare n varchar (50);
-declare cur cursor for select EmployeeID,First_Name from employee_salary;
-declare continue handler for not found set _cur1Done = TRUE;
-open cur;
-fetch cur into i,n;
-select i,n;
-close cur;
-end //
-delimiter ;
+declare sal int;
+declare nm varchar(50);
+declare v_finish int default 0;
+declare cur_1 cursor for select Salary,First_Name from employee_salary where Salary>15000;
+declare continue handler for not found set v_finish=1;
+open cur_1;
+get_stud : loop
+fetch cur_1 into sal,nm;
+select sal,nm;
+if v_finish = 1 then
+leave get_stud;
+end if;
+end loop get_stud;
+end$$
